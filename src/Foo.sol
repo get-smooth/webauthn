bd9c79710b1f5df82797b8fc12cdc26242364c4e
qd-qd <qdqdqdqdqd@protonmail.com>
2023-07-05 17:30:26 +0200

    🔥 remove template files

--- src/Foo.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.19 <0.9.0;
-
-contract Foo {
-    function id(uint256 value) external pure returns (uint256) {
-        return value;
-    }
-}
