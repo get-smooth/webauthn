eba17bf87e0d0189427931c4df2ed313de365f7a
qd-qd <qdqdqdqdqd@protonmail.com>
2023-08-04 21:12:34 +0200

    ♻️ Refactor the WebAuthn library
    Write a assembly-less solution for the WebAuthn verification which is
    more gas efficient and easier to read.
    ⚠️ This solution assumes the challenge is sent to the contract encoded
    in Base64.

--- src/WebAuthn.sol
+++ src/WebAuthn.sol
@@ -1,75 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19 <0.9.0;
 
-import { Base64 } from "../lib/solady/src/utils/Base64.sol";
+// import { Base64 } from "../lib/solady/src/utils/Base64.sol";
 import { ECDSA256r1 } from "../lib/secp256r1-verify/src/ECDSA256r1.sol";
 
 error InvalidAuthenticatorData();
 error InvalidClientData();
 
+/// dev: this implementation assumes the caller check if User Presence (0x01) or User Verification (0x04) are set
 library WebAuthn {
-    function format(
-        bytes calldata authenticatorData,
-        bytes1 authenticatorDataFlagMask,
-        bytes calldata clientData,
-        bytes32 clientChallenge,
-        uint256 clientChallengeOffset
-    )
-        internal
-        pure
-        returns (bytes32 result)
-    {
-        // Let the caller check if User Presence (0x01) or User Verification (0x04) are set
-        {
-            if ((authenticatorData[32] & authenticatorDataFlagMask) != authenticatorDataFlagMask) {
-                revert InvalidAuthenticatorData();
-            }
-            // Verify that clientData commits to the expected client challenge
-            string memory challengeEncoded = Base64.encode(abi.encodePacked(clientChallenge), true, true);
-            bytes memory challengeExtracted = new bytes(
-                bytes(challengeEncoded).length
-            );
-
-            assembly {
-                calldatacopy( // copy from calldata to memory
-                    add(challengeExtracted, 32), // destOffset
-                    add(clientData.offset, clientChallengeOffset), // offset
-                    mload(challengeExtracted) // size
-                )
-            }
-
-            bytes32 moreData; //=keccak256(abi.encodePacked(challengeExtracted));
-            assembly {
-                moreData := keccak256(add(challengeExtracted, 32), mload(challengeExtracted))
-            }
-            if (keccak256(abi.encodePacked(bytes(challengeEncoded))) != moreData) {
-                revert InvalidClientData();
-            }
-        }
-
-        // Verify the signature over sha256(authenticatorData || sha256(clientData))
-        bytes memory verifyData = new bytes(authenticatorData.length + 32);
-        assembly {
-            calldatacopy( // copy from calldata to memory
-                add(verifyData, 32), // destOffset
-                authenticatorData.offset, // offset
-                authenticatorData.length // size
-            )
-        }
-        bytes32 more = sha256(clientData);
-        assembly {
-            mstore(add(verifyData, add(authenticatorData.length, 32)), more)
-        }
-
-        return sha256(verifyData);
-    }
-
-    /// note: this implementation assumes the caller check if User Presence (0x01) or User Verification (0x04) are set
     function verify(
-        bytes calldata authenticatorData,
         bytes1 authenticatorDataFlagMask,
+        bytes calldata authenticatorData,
         bytes calldata clientData,
-        bytes32 clientChallenge,
+        bytes calldata clientChallengeBase64,
         uint256 clientChallengeOffset,
         uint256 r,
         uint256 s,
@@ -79,9 +23,28 @@ library WebAuthn {
         internal
         returns (bool)
     {
-        bytes32 message =
-            format(authenticatorData, authenticatorDataFlagMask, clientData, clientChallenge, clientChallengeOffset);
+        unchecked {
+            // Let the caller check if User Presence (0x01) or User Verification (0x04) are set
+            if ((authenticatorData[32] & authenticatorDataFlagMask) != authenticatorDataFlagMask) {
+                revert InvalidAuthenticatorData();
+            }
 
-        return ECDSA256r1.verify(message, r, s, qx, qy);
+            // TODO: Pass non-encode challenge? Convert the challenge to `bytes memory` and encode it to Base64
+            // bytes memory challengeEncoded = bytes(Base64.encode(abi.encodePacked(clientChallenge), true, true));
+
+            // Extract the challenge from the client data and hash it
+            bytes32 challengeHashed =
+                keccak256(clientData[clientChallengeOffset:(clientChallengeOffset + clientChallengeBase64.length)]);
+
+            // hash the encoded challenge and check both challenges are equal
+            if (keccak256(clientChallengeBase64) != challengeHashed) {
+                revert InvalidClientData();
+            }
+
+            // Verify the signature over sha256(authenticatorData || sha256(clientData))
+            bytes32 message = sha256(abi.encodePacked(authenticatorData, sha256(clientData)));
+
+            return ECDSA256r1.verify(message, r, s, qx, qy);
+        }
     }
 }
