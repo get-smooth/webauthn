2dd985bd002f3d73ab7a0e380a187eec7393bbdd
qd-qd <qdqdqdqdqd@protonmail.com>
2023-11-07 11:49:18 +0100

    ⚰️ remove code related to precomputed variants
    As the precompute variant is discontinued by the secp256r1-verify library,
    we can remove the code related to it.

--- src/WebAuthn256r1.sol
+++ src/WebAuthn256r1.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.8.19 <0.9.0;
 
 import { ECDSA256r1 } from "../lib/secp256r1-verify/src/ECDSA256r1.sol";
-import { ECDSA256r1Precompute } from "../lib/secp256r1-verify/src/ECDSA256r1Precompute.sol";
 import { WebAuthnBase } from "./WebAuthnBase.sol";
 
 /// @title WebAuthn256r1
@@ -30,29 +29,6 @@ contract WebAuthn256r1 is WebAuthnBase {
             );
 
             return ECDSA256r1.verify(message, r, s, qx, qy);
-        }
-    }
-
-    /// @notice Verify ECDSA signature though WebAuthn on the secp256r1 curve using a precomputed table
-    function verify(
-        bytes1 authenticatorDataFlagMask,
-        bytes calldata authenticatorData,
-        bytes calldata clientData,
-        bytes calldata clientChallenge,
-        uint256 clientChallengeOffset,
-        uint256 r,
-        uint256 s,
-        address precomputedTable
-    )
-        external
-        returns (bool)
-    {
-        unchecked {
-            bytes32 message = generateMessage(
-                authenticatorDataFlagMask, authenticatorData, clientData, clientChallenge, clientChallengeOffset
-            );
-
-            return ECDSA256r1Precompute.verify(message, r, s, precomputedTable);
         }
     }
 }
